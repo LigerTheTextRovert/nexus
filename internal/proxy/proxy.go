2b7a6f819da84f681450fe4db856e9863ab9c001
LiGER <samarghandi112w34@gmail.com>
2026-02-13 19:38:58 +0330

    feat: Add logs to proxy

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -3,6 +3,7 @@ package proxy
 
 import (
 	"io"
+	"log"
 	"net/http"
 
 	"github.com/LigerTheTextRovert/nexus/internal/config"
@@ -13,19 +14,26 @@ func ProxyHandler(w http.ResponseWriter, r *http.Request, c *config.Config) {
 
 	proxyRequest.Header = r.Header.Clone()
 
+	log.Printf("[PROXY] Frowarding to:%s", c.BackendURL+r.URL.Path)
+
 	client := &http.Client{}
 	res, err := client.Do(proxyRequest)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("[ERROR] An error occur: %s", err.Error())
 		return
 	}
+	log.Printf("[RESPONSE] Backend: %d, %s", res.StatusCode, http.StatusText(res.StatusCode))
 
 	for key, values := range res.Header {
 		for _, value := range values {
 			w.Header().Add(key, value)
 		}
 	}
+
 	w.WriteHeader(res.StatusCode)
+	log.Printf("[RESPONSE] Client: %d, %s", res.StatusCode, http.StatusText(res.StatusCode))
 
 	io.Copy(w, res.Body)
 	defer res.Body.Close()
