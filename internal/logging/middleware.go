505005b06838178e180f4a08fd0977d8faf28989
LiGER <samarghandi112w34@gmail.com>
2026-02-13 19:45:35 +0330

    refactor: Add logs for the header, method

--- internal/logging/middleware.go
+++ internal/logging/middleware.go
@@ -6,11 +6,22 @@ import (
 	"time"
 )
 
-func LoggerMiddleware(h http.Handler) http.Handler {
+func LoggingMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
+
+		log.Printf("[REQUEST] %s  %s", r.Method, r.URL.Path)
+
+		for key, values := range r.Header {
+			for _, value := range values {
+				log.Printf("[HEADER] %s : %s", key, value)
+			}
+		}
+
 		h.ServeHTTP(w, r)
+
 		duration := time.Since(startTime)
-		log.Printf("Request took %vs", duration)
+
+		log.Printf("[DURATION] Request took %v", duration)
 	})
 }
