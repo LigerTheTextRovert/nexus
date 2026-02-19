14539a7aaf30dbc5aab61d1b1fb048b187d57f27
LiGER <samarghandi112w34@gmail.com>
2026-02-19 14:28:45 +0330

    refactor: Refactor the ProxyHandler

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -4,22 +4,10 @@ package proxy
 import (
 	"net/http"
 	"net/http/httputil"
-	"net/url"
-	"time"
 )
 
-type App struct {
-	TargetURL string
-}
-
-func (a *App) ProxyHandler(w http.ResponseWriter, r *http.Request) {
-	targetURL, err := url.Parse(a.TargetURL)
-	if err != nil {
-		http.Error(w, "Proxy Error", http.StatusInternalServerError)
-	}
-	proxy := httputil.NewSingleHostReverseProxy(targetURL)
-	proxy.Transport = &http.Transport{ResponseHeaderTimeout: 10 * time.Second}
-
-	r.Host = targetURL.Host
-	proxy.ServeHTTP(w, r)
+func ProxyHandler(proxy *httputil.ReverseProxy) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	})
 }
