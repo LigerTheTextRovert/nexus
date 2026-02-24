38524ae27a622347d50c0ab6f6170d8a762d47c8
LiGER <samarghandi112w34@gmail.com>
2026-02-24 14:45:36 +0330

    fix: Normalize the path to make sure it starts with a /

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -10,6 +10,10 @@ import (
 func ProxyHandler(proxy *httputil.ReverseProxy, path string, stripPrefix bool) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if stripPrefix {
+			// We normalize the path if the incoming path doesn't start with /
+			if !strings.HasPrefix(path, "/") {
+				path = "/" + path
+			}
 			trimmed := strings.TrimPrefix(r.URL.Path, path)
 			if trimmed == "" {
 				trimmed = "/"
