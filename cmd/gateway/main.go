caf29bae53d02d2836fa926b8b92f58b7a4a01a4
LiGER <samarghandi112w34@gmail.com>
2026-04-14 15:34:04 +0330

    fix: added Content-Type to /health endpoint

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -27,6 +27,7 @@ func main() {
 	}
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.Write([]byte(`{"status" : "healthy"}`))
 	})
 
