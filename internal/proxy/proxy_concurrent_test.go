c67cb006a44aeddcc915c99d85b1a17a0ed92dea
LiGER <samarghandi112w34@gmail.com>
2026-04-16 00:40:35 +0330

    feat: impl concurrent test for proxyHandler

---
+++ internal/proxy/proxy_concurrent_test.go
@@ -0,0 +1,44 @@
+package proxy
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestProxyConcurrent(t *testing.T) {
+	const requestsNumber = 10000
+	var received int64
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		atomic.AddInt64(&received, 1)
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer backend.Close()
+
+	target, _ := url.Parse(backend.URL)
+	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxyHandler := ProxyHandler(proxy, "/api", true)
+
+	var wg sync.WaitGroup
+	wg.Add(requestsNumber)
+
+	for i := 0; i < requestsNumber; i++ {
+		go func() {
+			defer wg.Done()
+			req := httptest.NewRequest(http.MethodGet, "/api/users", nil)
+			rr := httptest.NewRecorder()
+			proxyHandler.ServeHTTP(rr, req)
+		}()
+	}
+
+	wg.Wait()
+
+	if received != requestsNumber {
+		t.Fatalf("error: expected %d requests, got %d", requestsNumber, received)
+	}
+
+}
